21e44a4a1a752c946eee65ae4a3721eb5fc29cdb
andrey zolotov <andrey.zolotov@nordclan.com>
2021-09-05 23:30:22 +0400

    Refactoring

---
+++ Assets/scripts/Skeleton/SkeletonSet.cs
@@ -0,0 +1,35 @@
+﻿namespace DeepMotion.DMBTDemo
+{
+    using UnityEngine;
+
+    //using Newtonsoft.Json.Linq;
+    [System.Serializable]
+    public class SkeletonSet
+    {
+        public Skeleton[] skeletons;
+
+
+
+        [System.Serializable]
+        public class Skeleton
+        {
+            [System.Serializable]
+            public class SkeletonJointDescriptor
+            {
+                public string type;
+                public int id;
+                public string node;
+
+            }
+
+            public string name;
+            public SkeletonJointDescriptor[] description;
+
+        }
+
+        public static SkeletonSet CreateFromJSON(string jsonString) {
+            return JsonUtility.FromJson<SkeletonSet>(jsonString);
+        }
+
+    }
+}
