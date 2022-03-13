bf69137019c7b66332adc82031995d9d824941c9
andrey zolotov <andrey.zolotov@nordclan.com>
2022-03-13 17:49:52 +0400

    Segmentation capture

--- Assets/scripts/MediapipeTracking/SelfieSegmentationCreator.cs
+++ Assets/scripts/MediapipeTracking/SelfieSegmentationCreator.cs
@@ -18,11 +18,14 @@ namespace Mediapipe.Unity.SelfieSegmentation
     [SerializeField] private RawImage tempSelfieImage;
 
     private Texture2D maskTexture;
+    private byte[] maskRGBA;
 
     protected override void OnStartRun() {
 
-      maskTexture = new Texture2D(maskWidth, maskHeight);
+      maskTexture = new Texture2D(maskWidth, maskHeight, TextureFormat.RGBA32, false);
       tempSelfieImage.texture = maskTexture;
+      maskRGBA = new byte[maskHeight * maskWidth*4];
+
       graphRunner.OnSelfieMaskOutput.AddListener(_selfieMaskAnnotationController.DrawLater);
       SetupAnnotationController(_selfieMaskAnnotationController, ImageSourceProvider.ImageSource);
       _selfieMaskAnnotationController.InitScreen();
@@ -65,16 +68,18 @@ namespace Mediapipe.Unity.SelfieSegmentation
     }
 
     private IEnumerator Test() { 
-      yield break;
+      //yield break;
       while (true) {
-        yield return ProcessImage(true);
+        CaptureSelfieToTexture();
+        yield return new WaitForEndOfFrame();
+        //yield return ProcessImage(true);
       }
     } 
     
     public void CaptureSegmentation() {
-      ProcessImageSync();
+      //ProcessImageSync();
 
-     // CaptureSelfieToTexture();
+      CaptureSelfieToTexture();
       //StartCoroutine(CaptureSegmentationCoroutine());
     }
 
@@ -100,10 +105,51 @@ namespace Mediapipe.Unity.SelfieSegmentation
       var mask = graphRunner.TryGetNext(out var _, true, false);
       if (mask != null) {
         //maskTexture.LoadRawTextureData(mask.GetPixels32());
-        maskTexture.SetPixels32(mask.GetPixels32());
+        //maskTexture.SetPixels32(mask.GetPixels32());
+
+        ConvertMask(mask);
       }
 
       return null;
+
+    }
+
+    private void ConvertMask(ImageFrame mask) {
+
+      int sz = mask.ChannelSize();
+      int w = mask.Width();
+      int h = mask.Height();
+      int count = w * h;
+
+      var floatArray = mask.CopyToFloatBuffer(w * h);
+
+      unsafe {
+        fixed (float *ptr = floatArray){
+          fixed (byte *outPtr = maskRGBA) {
+            var p1 = outPtr;
+            var p2 = ptr;
+            for (var i = 0; i < count; ++i) {
+              byte v = (byte)(*p2 * 255);
+              /**p1++ = 255;
+              *p1++ = 255;
+              *p1++ = v;
+              *p1++ = 255;
+              **/
+              p1[0] = v;
+              p1[1] = v;
+              p1[2] = v;
+              p1[3] = 255;
+              p1 += 4;
+              ++p2;
+            }
+          }
+
+        }
+      }
+      maskTexture.LoadRawTextureData(maskRGBA);
+      maskTexture.Apply();
+      tempSelfieImage.texture = maskTexture;
+
 
     }
   }
